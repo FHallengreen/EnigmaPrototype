692345587560d10c5d37d4acdb0dea5c145c499c
FHallengreen <fred70p9@stud.kea.dk>
2022-02-28 19:12:18 +0100

    decrypt

--- enigmaPrototype/src/Main.java
+++ enigmaPrototype/src/Main.java
@@ -16,13 +16,29 @@ public class Main {
 return cipherText;
   }
 
+  public String decryption (String message, int shiftValue) {
+    message = message.toUpperCase();
+    String cipherText = " ";
+    for (int i = 0; i < message.length(); i++) {
+      int charPos = ALPHABET.indexOf(message.charAt(i));
+      int keyValue = (charPos - shiftValue) % 29;
+      if (keyValue < 0){
+        keyValue = ALPHABET.length() + keyValue;
+      }
+      char replaceValue = ALPHABET.charAt(keyValue);
+      cipherText += replaceValue;
+    }
+    return cipherText;
+  }
+
   public static void main(String[] args) {
     Main obj = new Main();
     Scanner scan = new Scanner(System.in);
-    System.out.println("Enter text you wish to encrypt: ");
+    System.out.println("Enter text you wish to encrypt/decrypt: ");
     String message = scan.nextLine();
     System.out.println("Enter Shift Value: ");
     int key = scan.nextInt();
-    System.out.println("Encrypted message: " + obj.encrypt(message, key));
+//    System.out.println("Encrypted message: " + obj.encrypt(message, key));
+    System.out.println("Decrypted message: " + obj.decryption(message,key));
   }
 }
