1b49beb1bae724f19d6b1f31f0522d0195e138ab
FHallengreen <fred70p9@stud.kea.dk>
2022-02-28 18:42:51 +0100

    encrypt

--- enigmaPrototype/src/Main.java
+++ enigmaPrototype/src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
+  final String ALPHABET ="ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
 
+  public String encrypt(String message, int shiftValue) {
+    message = message.toUpperCase();
+    String cipherText = " ";
+    for (int i = 0; i < message.length(); i++) {
+        int charPos = ALPHABET.indexOf(message.charAt(i));
+        int keyValue = (shiftValue + charPos) % 29;
+        char replaceValue = ALPHABET.charAt(keyValue);
+        cipherText += replaceValue;
+    }
+return cipherText;
+  }
+
+  public static void main(String[] args) {
+    Main obj = new Main();
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter text you wish to encrypt: ");
+    String message = scan.nextLine();
+    System.out.println("Enter Shift Value: ");
+    int key = scan.nextInt();
+    System.out.println("Encrypted message: " + obj.encrypt(message, key));
   }
 }
