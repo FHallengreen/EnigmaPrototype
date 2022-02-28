2e9f0843ac7a72ad53524151d17f02ca7e31c0ac
FHallengreen <fred70p9@stud.kea.dk>
2022-02-28 19:34:26 +0100

    encrypt og decrypt NU med mellemrum

--- enigmaPrototype/src/Main.java
+++ enigmaPrototype/src/Main.java
@@ -2,28 +2,34 @@ import java.util.Scanner;
 
 public class Main {
 
-  final String ALPHABET ="ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+  final String ALPHABET =" ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
 
   public String encrypt(String message, int shiftValue) {
     message = message.toUpperCase();
-    String cipherText = " ";
+    String cipherText = "";
     for (int i = 0; i < message.length(); i++) {
         int charPos = ALPHABET.indexOf(message.charAt(i));
         int keyValue = (shiftValue + charPos) % 29;
+      if (charPos == 0){
+        keyValue = 0;
+      }
         char replaceValue = ALPHABET.charAt(keyValue);
         cipherText += replaceValue;
     }
 return cipherText;
   }
 
-  public String decryption (String message, int shiftValue) {
+  public String decrypt (String message, int shiftValue) {
     message = message.toUpperCase();
-    String cipherText = " ";
+    String cipherText = "";
     for (int i = 0; i < message.length(); i++) {
       int charPos = ALPHABET.indexOf(message.charAt(i));
       int keyValue = (charPos - shiftValue) % 29;
-      if (keyValue < 0){
-        keyValue = ALPHABET.length() + keyValue;
+      if (charPos == 0){
+        keyValue = 0;
+      }
+      else if (keyValue < 0){
+        keyValue = ALPHABET.length() + keyValue -1;
       }
       char replaceValue = ALPHABET.charAt(keyValue);
       cipherText += replaceValue;
@@ -38,7 +44,5 @@ return cipherText;
     String message = scan.nextLine();
     System.out.println("Enter Shift Value: ");
     int key = scan.nextInt();
-//    System.out.println("Encrypted message: " + obj.encrypt(message, key));
-    System.out.println("Decrypted message: " + obj.decryption(message,key));
   }
 }
